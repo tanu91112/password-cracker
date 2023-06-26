c13f6675515be1599422861e3da93666ad8401c9
Tanu Chandravanshi <134772713+tanu91112@users.noreply.github.com>
2023-06-26 10:30:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import hashlib
+
+pass_found=0
+
+i_hash = input("Enter the hashed password:")
+p_doc = input("\nEnter password filename including path:")
+
+p_file = open(p_doc, 'r')
+
+for word in p_file:
+    enc_word = word.encode('utf-8')
+    hash_word=hashlib.md5(enc_word.strip())
+    digest=hash_word.hexdigest()
+    
+    if digest == i_hash:
+        print("Password found : ", word)
+        pass_found=1
+        break
+        
+if not pass_found:
+    print("Password not found")
